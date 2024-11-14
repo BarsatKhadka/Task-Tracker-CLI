a65292bef0056c354fe4de772822359dd94ef35a
Barsat khadka <khadkabarsat598@gmail.com>
2024-11-13 23:03:20 -0600

    Input verification uusing regex pattern matching

---
+++ src/main/java/org/TaskTracker/CheckTaskName.java
@@ -0,0 +1,35 @@
+package org.TaskTracker;
+
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class CheckTaskName {
+    ArrayList<String> allCommands = new ArrayList<>(Arrays.asList("add", "update" , "delete",
+            "list", "mark-in-progress", "mark-done"));
+
+    String user_command = "";
+
+
+    public CheckTaskName(){
+
+    }
+
+    public CheckTaskName(String user_command) {
+        this.user_command = user_command;
+
+    }
+
+    public boolean verifyValid() {
+        for (String command : allCommands) {
+            String regex = "^" + command + " .+";
+            Pattern pattern = Pattern.compile(regex);
+            Matcher matcher = pattern.matcher(user_command);
+            return matcher.matches();
+        }
+        return false;
+    }
+
+}
